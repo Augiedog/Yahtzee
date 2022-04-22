6a8e52fab6819cb3efddf7e62232018b20561053
Augie Bren <Augie>
2022-04-21 22:24:47 -0500

    working on dice count

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let playArea = document.querySelector('#play-area')
 let hold = document.querySelector('#hand')
-let diceCount = document.getElementsByTagName('img')
+let dice = 0
 
 function Dice(url){
     let image = document.createElement('img')
@@ -23,11 +23,24 @@ function Dice(url){
     return image
 }
 
+function diceCount() {
+    var diceLimit = 5
+    if (dice > diceLimit) {
+        // trying to remove the dice in the play area and; roll the amount left in the play area
+        // document.querySelector(div.img).remove(dice-diceLimit)
+        alert(`More than 5 dice!!`)
+    } 
+}
+
 // Roll Button 
 document.getElementById('roll').addEventListener('click', () => {
-    for (let i = 0; i < 5; i++) {
+    for ( i = 0; i < 5; i++) {
         let rolledValue = Math.ceil(Math.random() * 6)
         // callBack funtion creats the image of a dice
         Dice(rolledValue)
-    }         
+        dice++
+        
+    }   
+    diceCount()
 })
+
