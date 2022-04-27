750e1bccd5175f6db59f0009d9621a3acd71d7ef
Augie Bren <Augie>
2022-04-26 22:27:26 -0500

    a good night

--- scorecard.js
+++ scorecard.js
@@ -19,6 +19,33 @@ let totalScore = document.getElementById('total-score')
 
 function ScoreCard(dieInHand) {
     // need to fill the Score Card
+    // dieInHand.sort()
+    // function countAces(aces) {
+    //     aces.reduce
+    //     return 
+    // }
+
+    for ( let i = 0; i < dieInHand.length; i++) {
+        if (dieInHand[i] === 1) {
+            acesCount.textContent = dieInHand
+        }
+        if (dieInHand[i] === 2) {
+            twosCount.textContent = dieInHand
+        }
+        if (dieInHand[i] === 3) {
+            threesCount.textContent = dieInHand
+        }
+        if (dieInHand[i] === 4) {
+            foursCount.textContent = dieInHand
+        }
+        if (dieInHand[i] === 5) {
+            fivesCount.textContent = dieInHand
+        }
+        if (dieInHand[i] === 6) {
+            sixesCount.textContent = dieInHand
+        }
+    }
+    
 
     chance.textContent = dieInHand.reduce((a, b) => a + b, 0)
     
